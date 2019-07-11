cc8394d4f7a3620d7212d1afd795408b8e959cb3
bachir ali <bachir.ali@i2advert.com>
2019-07-11 09:45:18 +0100

    config app.name

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="container">
-        <h2>What is Laracarte?</h2>
-        <p>Laracarte is a clone app of <a href="https://laramap.com" target="_blank">Laramap.com</a></p>    
+        <h2>What is {{config('app.name')}}?</h2>
+        <p>{{config('app.name')}} is a clone app of <a href="https://laramap.com" target="_blank">Laramap.com</a></p>    
         
         <div class="row">
             <div class="col-md-6">
@@ -16,10 +16,10 @@
         <p>Feel free to help to improve the <a href="https://github.com/mb151/laracarte-tdn" target="_blank">Source Code</a>.</p>
         <hr>
         <h2>What is Laramap?</h2>
-        <p>Laramap is the website by which Laracarte was inspired :).</p>
+        <p>Laramap is the website by which {{config('app.name')}} was inspired :).</p>
         <p>More info <a href="https://laramap.com/p/about">here</a>.</p>
         <hr>
-        <h2>Which tools and services are used in Laracarte?</h2>
+        <h2>Which tools and services are used in {{config('app.name')}}?</h2>
         <p>Basically it's built on Laravel &amps; Bootstrap. But there's a bunch of services used for email and other sections.</p>
         <ul>
             <li>Laravel</li>
